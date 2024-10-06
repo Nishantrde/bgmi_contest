9721a519b3c0ccc85422db4103d66d30c8efb09a
Nishant <nishant2024rde@gmail.com>
2024-10-06 10:06:35 -0700

    Initial Commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,3 @@
-# Inside views.py
 from django.shortcuts import render, redirect
 from .forms import TeamForm
 
@@ -7,8 +6,8 @@ def index(request):
         form = TeamForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')  # Replace 'success_url' with the URL where you want to redirect after form submission
+            return redirect('/')
     else:
         form = TeamForm()
-    
+
     return render(request, 'index.html', {'form': form})
