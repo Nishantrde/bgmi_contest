f0dcbbd35e94d32ed156d8e12de6bd6583165147
Nishant <nishant2024rde@gmail.com>
2024-10-05 20:41:16 -0700

    Initial Commit

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -5,4 +5,4 @@ from .models import Team
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ['team_name', 'member_1', 'member_2', 'member_3', 'member_4', 'ph_no']
+        fields = ['team_name', 'player_1', 'player_2', 'player_3', 'player_4', 'ph_no']
--- mainapp/models.py
+++ mainapp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Team(models.Model):
     team_name = models.CharField(default="", max_length=100)
-    member_1 = models.CharField(default="", max_length=100)
-    member_2 = models.CharField(default="", max_length=100)
-    member_3 = models.CharField(default="", max_length=100)
-    member_4 = models.CharField(default="", max_length=100)
+    player_1 = models.CharField(default="", max_length=100)
+    player_2 = models.CharField(default="", max_length=100)
+    player_3 = models.CharField(default="", max_length=100)
+    player_4 = models.CharField(default="", max_length=100)
     ph_no = models.CharField(default="", max_length=100)
 
     def __str__(self):
