ccc4fef17c2c56b68634573d9e7c566e87f9a717
Nishant <nishant2024rde@gmail.com>
2024-10-06 19:41:48 -0700

    Initial Commit

--- bgmi_contest/urls.py
+++ bgmi_contest/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("mainapp.urls")),
+    path("google_sso/", include("django_google_sso.urls", namespace="django_google_sso")),
 ]
